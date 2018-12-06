5498c745a16a5e4ad87cac22326b6ebd19ce75ab
zhangke8 <19340295+zhangke8@users.noreply.github.com>
2018-12-06 13:15:45 -0500

    converting to generic toggle

--- JSSForm/src/app/form/form.component.ts
+++ JSSForm/src/app/form/form.component.ts
@@ -9,7 +9,7 @@ export class FormComponent implements OnInit {
   options = [];
   names = [];
   productTypes = ["blah"];
-
+  objDate = Date.now();
   constructor() {
     this.options = ["Customer", "Supplier"];
     this.names = [];
@@ -18,22 +18,14 @@ export class FormComponent implements OnInit {
   public show1: boolean = false;
   public show2: boolean = false;
   public show3: boolean = false;
-  public selectedVal: string;
-  public hideable: boolean = false;
 
-  objDate = Date.now();
-  toggle1() {
-    this.show1 = !this.show1;
+  toggle(element) {
+      this[element] = !this[element];
   }
 
-  toggle2() {
-    this.show2 = !this.show2;
-  }
-
-  toggle3() {
-    this.show3 = !this.show3;
-  }
-
+   // hiding dropdowns or input boxes
+   public selectedVal: string;
+   public hideable: boolean = false;
   onRowClick() {
     if (this.selectedVal === "Customer") {
       this.hideable = true;
